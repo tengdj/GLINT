596dde26c3f44f8199ed49c9b63d5f018107590a
tengdj <tengdj@vip.qq.com>
2021-02-07 19:26:15 -0500

    minor update

--- src/tracing/trace_generator.cpp
+++ src/tracing/trace_generator.cpp
@@ -241,6 +241,7 @@ Point *trace_generator::generate_trace(){
 	tctx.target[0] = (void *)this;
 	tctx.target[1] = (void *)ret;
 	tctx.num_objects = config.num_objects;
+	tctx.report_gap = 1;
 	for(int i=0;i<config.num_threads;i++){
 		pthread_create(&threads[i], NULL, gentrace, (void *)&tctx);
 	}
