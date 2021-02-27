d8f9565d775b89a4a9c00b69aa1ef6d81797613b
tengdj <tengdj@vip.qq.com>
2021-02-27 15:22:34 -0500

    debuging

--- src/cuda/geometry.cu
+++ src/cuda/geometry.cu
@@ -451,7 +451,7 @@ void cuda_update_schema_collect(workbench *bench){
 		if(leafchild==4&&ncounter<bench->config->grid_capacity){
 			// the children of this node need be deallocated
 			if(++bench->schema[curnode].underflow_count>=bench->config->schema_update_delay){
-				printf("%d\n",curnode);
+				//printf("%d\n",curnode);
 				uint sidx = atomicAdd(&bench->lookup_stack_index[0],1);
 				bench->lookup_stack[0][sidx] = curnode;
 				bench->schema[curnode].underflow_count = 0;
--- src/tracing/partition.cpp
+++ src/tracing/partition.cpp
@@ -169,7 +169,7 @@ void workbench::split_node(uint cur_node){
 	assert(schema[cur_node].type==LEAF);
 	lock();
 
-	//printf("%d\n",cur_node);
+	printf("%d\n",cur_node);
 	//schema[cur_node].mbr.print();
 	schema[cur_node].type = BRANCH;
 	grids_stack[--grids_stack_index] = schema[cur_node].grid_id;
